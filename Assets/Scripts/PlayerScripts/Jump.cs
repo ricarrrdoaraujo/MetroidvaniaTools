9874c35e73be2249f72df1a3801866d6c163a7db
Ricardo Araujo <ricarrrdoluigi@gmail.com>
2021-03-13 20:10:56 -0300

    Limiting the jump and fall speed

--- Assets/Scripts/PlayerScripts/Jump.cs
+++ Assets/Scripts/PlayerScripts/Jump.cs
@@ -7,9 +7,13 @@ namespace MetroidvaniaTools
 {
     public class Jump : Abilities
     {
+        [SerializeField] protected bool limitAirJumps;
         [SerializeField] protected int maxJumps;
         [SerializeField] protected float jumpForce;
         [SerializeField] protected float distanceToCollider;
+        [SerializeField] protected float maxJumpSpeed;
+        [SerializeField] protected float maxFallSpeed;
+        [SerializeField] protected float acceptedFallSpeed;
         [SerializeField] private LayerMask collisionLayer;
 
         private bool isJumping;
@@ -31,6 +35,12 @@ namespace MetroidvaniaTools
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 if (!character.isGrounded && numberOfJumpsLeft == maxJumps)
+                {
+                    isJumping = false;
+                    return false;
+                }
+
+                if (limitAirJumps && Falling(acceptedFallSpeed))
                 {
                     isJumping = false;
                     return false;
@@ -63,15 +73,34 @@ namespace MetroidvaniaTools
             {
                 character.isGrounded = false;
                 isJumping = false;
+                if (Falling(0) && rb.velocity.y < maxFallSpeed)
+                { 
+                    rb.velocity = new Vector2(rb.velocity.x, maxFallSpeed);
+                }
             }
-                
+        }
+
+        protected virtual bool Falling(float velocity)
+        {
+            if (!isGrounded && rb.velocity.y < velocity)
+            {
+                return true;
+            }
+            else
+                return false;
         }
 
         protected virtual void IsJumping()
         {
             if (isJumping)
             {
+                rb.velocity = new Vector2(rb.velocity.x, 0);
                 rb.AddForce(Vector2.up * jumpForce);
+            }
+
+            if (rb.velocity.y > maxJumpSpeed)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, maxJumpSpeed);
             }
         }
     }
